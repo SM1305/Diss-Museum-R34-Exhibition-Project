f3b975d5c1020349ae61f9a8d4f94cc9af1552a7
ElliotChester <elliotchester97@gmail.com>
2018-08-24 18:03:57 +0100

    Added the Prototype for the app
    Prototype including interactive screen layouts

---
+++ FirstPrototype/DissMuseumPrototype/Assets/MenuScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuScript : MonoBehaviour {
+
+
+	public void OpenMenu(GameObject MenuToOpen)
+	{
+		MenuToOpen.SetActive(true);
+	}
+
+	public void CloseMenu(GameObject MenuToClose)
+	{
+		MenuToClose.SetActive(false);
+	}
+}
