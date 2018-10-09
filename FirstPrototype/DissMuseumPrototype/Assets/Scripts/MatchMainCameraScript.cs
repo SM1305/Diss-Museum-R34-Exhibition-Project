4be971ca6ac4237accda016c3567423e73738e66
Elliot Chester <elliotchester@outlook.com>
2018-10-09 14:10:15 +0100

    updated prototype with inset object
    updated the prototype with the inset object that can be used to quickly create the final inset events

---
+++ FirstPrototype/DissMuseumPrototype/Assets/Scripts/MatchMainCameraScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MatchMainCameraScript : MonoBehaviour {
+
+    public GameObject mainCamera;
+
+    private void Start()
+    {
+
+    }
+
+    void LateUpdate()
+    {
+        if (this.GetComponent<Camera>().fieldOfView != mainCamera.GetComponent<Camera>().fieldOfView)
+        {
+            this.GetComponent<Camera>().fieldOfView = mainCamera.GetComponent<Camera>().fieldOfView;
+        }
+
+        this.transform.position = mainCamera.transform.position;
+        this.transform.eulerAngles = mainCamera.transform.eulerAngles;
+    }
+}
---
+++ FirstPrototype/DissMuseumPrototype/Assets/Scripts/cameraTextureSetup.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraTextureSetup : MonoBehaviour
+{
+
+    public Camera targetCamera;
+    public Material material;
+
+    // Use this for initialization
+    void Start()
+    {
+        if (targetCamera.targetTexture != null)
+        {
+            targetCamera.targetTexture.Release();
+        }
+        targetCamera.targetTexture = new RenderTexture(Screen.width, Screen.height, 24);
+        material.mainTexture = targetCamera.targetTexture;
+    }
+}
