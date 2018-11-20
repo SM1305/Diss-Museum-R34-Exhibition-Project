53981d020cc52a0cabb2d9be7ec858e2822490f9
Tom <tom.gibbs@hotmail.co.uk>
2018-11-20 15:18:35 +0000

    Henry's changes 20/11/18

---
+++ FirstPrototype/DissMuseumPrototype/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+    GameObject r34;
+
+	// Use this for initialization
+	void Start () {
+        r34 = GameObject.FindGameObjectWithTag("R34");
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+        transform.position = r34.transform.position;
+	}
+}
