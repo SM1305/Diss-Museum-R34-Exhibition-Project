cc208dda0bd198ae5a7207d9cf3c36860c044ad4
Tom <tom.gibbs@hotmail.co.uk>
2018-12-18 16:26:00 +0000

    Information screen object rotation restricted to Y-axis rotation only

--- FirstPrototype/DissMuseumPrototype/Assets/Scripts/ObjectTouchRotate.cs
+++ FirstPrototype/DissMuseumPrototype/Assets/Scripts/ObjectTouchRotate.cs
@@ -7,10 +7,18 @@ public class ObjectTouchRotate : MonoBehaviour
     public float rotateSpeed;
     public GameObject objectCamera;
 
+
+    //uses Unity event component
+    //during touch, rotates object based on touch input
     public void FingerRotate()
     {
+        ///
+        /// uncomment lines for full 3D rotation
+        /// input currently restricted to only allow horizontal rotation (about Y axis)
+        ///
+
         float Xrot = Input.GetAxis("Mouse X") * rotateSpeed * Mathf.Deg2Rad;
-        float Yrot = Input.GetAxis("Mouse Y") * rotateSpeed * Mathf.Deg2Rad;
+        //float Yrot = Input.GetAxis("Mouse Y") * rotateSpeed * Mathf.Deg2Rad;
 
         if (Vector3.Dot(transform.up, Vector3.up) >= 0)
         {
@@ -21,13 +29,13 @@ public class ObjectTouchRotate : MonoBehaviour
             transform.Rotate(Vector3.up, -Xrot, Space.World);
         }
 
-        if (Vector3.Dot(-transform.right, Vector3.left) >= 0)
-        {
-            transform.Rotate(Vector3.right, -Yrot, Space.World);
-        }
-        else
-        {
-            transform.Rotate(Vector3.right, -Yrot, Space.World);
-        }
+        //if (Vector3.Dot(-transform.right, Vector3.left) >= 0)
+        //{
+        //    transform.Rotate(Vector3.right, -Yrot, Space.World);
+        //}
+        //else
+        //{
+        //    transform.Rotate(Vector3.right, -Yrot, Space.World);
+        //}
     }
 }
