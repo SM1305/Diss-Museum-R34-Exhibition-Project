6940f42dba3eb4719aa9851ce1fd448f19a8007f
Henry Crofts <hwcrofts@gmail.com>
2018-12-19 23:41:14 +0000

    Updated prefabs with new audio system

---
+++ FirstPrototype/DissMuseumPrototype/Assets/TakeOffAudio.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TakeOffAudio : MonoBehaviour {
+
+    public AudioSource engine;
+	// Use this for initialization
+	void Start () {
+        engine.Play();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
