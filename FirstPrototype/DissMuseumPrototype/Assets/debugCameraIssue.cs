ce72153ebb094a0635f861769b1cbe213eb1e266
Elliot Chester <ElliotChester@outlook.com>
2019-07-12 22:31:06 +0100

    updated project for the event

---
+++ FirstPrototype/DissMuseumPrototype/Assets/debugCameraIssue.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class debugCameraIssue : MonoBehaviour
+{
+    public Transform arcamera;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.GetComponent<Text>().text = arcamera.position.ToString() + "+" + arcamera.eulerAngles.ToString();
+    }
+}
